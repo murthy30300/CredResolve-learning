ce883683960d529b437a1ab2f4a6bba1ec6b21f3
murthy30300 <2200030300cseh@gmail.com>
2026-02-11 18:50:06 +0530

    Add PROJECT3/sbdemo3 clean without secrets

---
+++ PROJECT3/sbdemo3/src/main/java/com/cr/sbdemo3/DemoController.java
@@ -0,0 +1,12 @@
+package com.cr.sbdemo3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class DemoController {
+    @GetMapping("/")
+    public int getDemo(){
+        return 5;
+    }
+}
---
+++ PROJECT3/sbdemo3/src/main/java/com/cr/sbdemo3/Sbdemo3Application.java
@@ -0,0 +1,13 @@
+package com.cr.sbdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbdemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbdemo3Application.class, args);
+    }
+
+}
